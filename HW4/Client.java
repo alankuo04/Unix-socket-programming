a98a26d5ea45bfb87c2ae71ecadde1274fb3d99e
alankuo04 <lcckuoo40@gmail.com>
2021-06-10 00:49:20 +0800

    final version.

--- HW4/Client.java
+++ HW4/Client.java
@@ -5,46 +5,63 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
-
 public class Client {
     public static void main(String[] args) throws Exception{
+        // sharing game for two thread
         TicTacToeClient game = new TicTacToeClient();
-        clientThread socketThread = new clientThread(game);
+        // first thread for sending message
+        clientThread socketThread = new clientThread(args[0], game);
+        // second thread for gui maintaining
         clientGUI lobby = new clientGUI(game);
         socketThread.start();
         lobby.start();
     }
 }
 
+// thread for sending message
 class clientThread extends Thread{
+    // sharing game for the thread
     private TicTacToeClient game;
-    public clientThread(TicTacToeClient game) {
+    private String ip = "localhost";
+    // constructor for setting the game
+    public clientThread(String ip, TicTacToeClient game) {
         this.game = game;
+        this.ip = ip;
     }
     public void run() {
         try{
+            // setting up the socket, data output stream and input stream
             String buffer = "", backBuffer = "";
-            Socket clientSocket = new Socket("192.168.91.128", 50005);
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            Socket clientSocket = new Socket(ip, 50005);
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            // getting data until "end" token
             while(!buffer.split(" ")[0].equals("end")){
+                // sending some garbage message to get the new message from server
                 buffer = game.getAndSetOutputString("l l");
-                System.out.println(buffer);
+                //System.out.println(buffer);
+                
                 outToServer.writeBytes(buffer + '\n');
                 backBuffer = inFromServer.readLine();
+                
+                // checking the state is lobby or room
                 if(backBuffer.split("/")[0].equals("room")){
+                    // get the message from server
+                    game.setMessage(backBuffer.split("/")[1]);
+                    if(game.getMessage().length()>=10 && game.getMessage().split(" ")[0].equals("Player"))
+                        game.setInGame(true);
+                    if(!game.getMessage().equals(" "))                
+                        game.setMessage(backBuffer.split("/")[1]);
                     game.setMap(backBuffer.split("/")[2]);
-                    game.setInGame(true);
                 }
                 else{
                     game.setInGame(false);
-                    game.setMessage("Message: "+backBuffer.split("/")[1]);
+                    game.setMessage(backBuffer.split("/")[1]);
                     if(backBuffer.split("/").length>=3)
                         game.setUserList(backBuffer.split("/")[2]);
                 }
-                System.out.println("FROM SERVER: " + backBuffer);
+                //System.out.println("FROM SERVER: " + backBuffer);
+                // sleep one second for not sending message too fast
                 sleep(1000);
             }
             System.out.println("Client end.");
@@ -56,6 +73,7 @@ class clientThread extends Thread{
     }
 }
 
+// gui thread
 class clientGUI extends Thread{
     private TicTacToeClient game;
     JFrame lobby, room;
@@ -67,23 +85,30 @@ class clientGUI extends Thread{
     boolean end = false;
     final int BUTTON_SIZE = 100;
 
+    // constructor to set the sharing game
     public clientGUI(TicTacToeClient game) {
         this.game = game;
     }
     public void run(){
+        // set two page to emulate two rooms
         lobby = new JFrame();
         room = new JFrame();
+        // initialize the gui button, label, ...
         initialize();
         try{
+            // renew the gui in one second
             while(!end){
-                if(game.isInGame() && !game.getMessage().equals("Playing room is full.")){
+                // in the lobby room
+                if(game.isInGame()){
                     lobby.setVisible(false);
                     room.setVisible(true);
                 }
+                // in the game room
                 else{
                     lobby.setVisible(true);
                     room.setVisible(false);    
                 }
+                // refreshing the gui button, label,...
                 refresh();
                 sleep(1000);
             }
@@ -92,6 +117,7 @@ class clientGUI extends Thread{
             e.printStackTrace();
         }
     }
+    // gui initialize function
     void initialize(){
         lobby.setTitle("Client");
         lobby.setLayout(null);
@@ -106,12 +132,16 @@ class clientGUI extends Thread{
         loginUserName.setBounds(130, 5, 200, 20);
         lobby.add(loginUserName);
 
+        // login button let server know a new player is going to login
         login = new JButton("Login");
         login.setBounds(330, 5, 100, 20);
         lobby.add(login);
+        // get the action of pressing the button
         login.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
+                // set the player name and change the gui into next mode
                 game.setPlayerName(loginUserName.getText());
+                room.setTitle(game.getPlayerName());
                 loginUserName.setVisible(false);
                 login.setVisible(false);
                 join.setVisible(true);
@@ -121,16 +151,18 @@ class clientGUI extends Thread{
             }
         });
         
+        // join button to play the game
         join = new JButton("Join");
         join.setBounds(330, 5, 100, 20);
         lobby.add(join);
         join.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                game.setInGame(true);
                 game.getAndSetOutputString("play l");
+                game.setMessage("Waiting the empty room to join...");
             }
         });
 
+        // logout button to shutdown the connection to socket
         logout = new JButton("Logout");
         logout.setBounds(330, 30, 100, 20);
         logout.setVisible(false);
@@ -151,6 +183,8 @@ class clientGUI extends Thread{
         seperateLine.setBounds(10, 50, 450, 20);
         lobby.add(seperateLine);
 
+        // online user list showing
+        // it can renew itself by getting the value in vector
         Vector<String> columns = new Vector<String>(Arrays.asList("User Name", "Win", "Lose", "Online"));
         onlineList = new JTable(game.getUserList(), columns);
         scrollPane = new JScrollPane(onlineList);
@@ -161,6 +195,7 @@ class clientGUI extends Thread{
         textInRoom.setBounds(10, 5, 300, 20);
         room.add(textInRoom);
 
+        // a lot of button in the game
         TL = new JButton("");
         TL.setBounds(10, 30, BUTTON_SIZE, BUTTON_SIZE);
         TL.setFont(new Font("Calibri", Font.PLAIN, 50));
@@ -250,7 +285,8 @@ class clientGUI extends Thread{
                 game.getAndSetOutputString("play 2,2");
             }
         });
-
+        
+        // set the window size
         lobby.setSize(450, 400);
         lobby.setVisible(true);
         room.setSize(330, 380);
@@ -259,11 +295,13 @@ class clientGUI extends Thread{
         lobby.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
         room.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
     }
+    // refreshing gui function
     void refresh(){
         if(game.checkWin()){
             game.setInGame(false);
         }
         else{
+            // in the game room, update the message of each button
             textInRoom.setText(game.getMessage());
             TL.setText(game.getMap(0,0));
             T.setText(game.getMap(0,1));
@@ -275,11 +313,19 @@ class clientGUI extends Thread{
             D.setText(game.getMap(2,1));
             DR.setText(game.getMap(2,2));
         }
-        onlineList.updateUI();
-        text.setText(game.getMessage());
+        // in the lobby
+        if(!game.isInGame()){
+            // update the online user list
+            onlineList.updateUI();
+            text.setText(game.getMessage());
+            // recommend the weak player to you
+            if(!game.getPlayerName().equals(""))
+                loginSign.setText("Hello, "+game.getPlayerName()+". Recommend "+game.recommend()+" as your opponent.");    
+        }
     }
 }
 
+// the sharing game class
 class TicTacToeClient{
     private String[][] map = {{"","",""},{"","",""},{"","",""}};
     private Vector<Vector<String>> userList = new Vector<>();
@@ -289,19 +335,21 @@ class TicTacToeClient{
     private String outputString = "l l";
     private String message = "";
 
+    // set the player name
     public void setPlayerName(String playerName){
         this.playerName = playerName;
     }
+    // get the player name
     public String getPlayerName(){
         return playerName;
     }
+    // set the user list by string getting from server
     public void setUserList(String data){
-        System.out.println("here:"+data);
         String[] temp = data.split(" ");
         for(int i=0;i<temp.length;i++){
             boolean alreadyIn = false;
             for(int j=0;j<userList.size();j++){
-                System.out.println(userList.get(j).get(0)+", "+temp[i].split(",")[0]);
+                //System.out.println(userList.get(j).get(0)+", "+temp[i].split(",")[0]);
                 if(userList.get(j).get(0).equals(temp[i].split(",")[0])){
                     alreadyIn = true;
                     userList.set(j, new Vector<String>(Arrays.asList(temp[i].split(","))));
@@ -311,15 +359,17 @@ class TicTacToeClient{
                 userList.add(new Vector<String>(Arrays.asList(temp[i].split(","))));
         }
     }
+    // get the user list
     public Vector<Vector<String>> getUserList(){
-        for(int i=0;i<userList.size();i++){
+        /*for(int i=0;i<userList.size();i++){
             for(int j=0;j<userList.get(i).size();j++){
                 System.out.print(userList.get(i).get(j)+ " ");
             }
             System.out.print("\n");
-        }
+        }*/
         return userList;
     }
+    // set the game room map
     public void setMap(String map){
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -327,26 +377,33 @@ class TicTacToeClient{
             }
         }
     }
+    // get the game room map by indicated position
     public String getMap(int row, int col){
         return map[row][col];
     }
+    // set the situation of the game
     public void setInGame(boolean inGame) {
         this.inGame = inGame;
     }
+    // get the situation of the game
     public boolean isInGame(){
         return inGame;
     }
+    // get the temporary output string, then set it into the new one
     public String getAndSetOutputString(String output){
         String temp = outputString;
         outputString = output;
         return temp;
     }
+    // get the message
     public String getMessage(){
         return message;
     }
+    // set the message
     public void setMessage(String message){
         this.message = message;
     }
+    // check the game is win or not
     public boolean checkWin(){
         if(map[0][0].equals("O") && map[0][1].equals("O") && map[0][2].equals("O") || map[1][0].equals("O") && map[1][1].equals("O") && map[1][2].equals("O") || map[2][0].equals("O") && map[2][1].equals("O") && map[2][2].equals("O"))
             return true;
@@ -362,5 +419,21 @@ class TicTacToeClient{
             return true;
         else
             return false;
+    }
+    // recommend the weak opponent to you
+    public String recommend(){
+        String weakOpponet = "";
+        float winRate = 1;
+        for(int i=0;i<userList.size();i++){
+            if(!userList.get(i).get(0).equals(playerName)){
+                float w = Float.parseFloat(userList.get(i).get(1));
+                float l = Float.parseFloat(userList.get(i).get(2));
+                if(w/l <= winRate){
+                    winRate = w/l;
+                    weakOpponet = userList.get(i).get(0);
+                }
+            }
+        }
+        return weakOpponet;
     }
 }
--- HW4/Server.java
+++ HW4/Server.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 
 public class Server {
     public static void main(String[] args) throws Exception{
+        // setting the socket for the server
         ServerSocket server = new ServerSocket(50005);
         Socket socket = null;
+        // the sharing game room
         TicTacToeGame game = new TicTacToeGame();
         try {
+            // loop for getting new connection
             while (true) {
+                // accept a new connection
                 socket = server.accept();
                 System.out.println("started: " + server);
+                // put it into a new thread, and put the sharing game too
                 serverThread socketThread = new serverThread(socket, game);
                 socketThread.start();
             }
@@ -23,6 +28,7 @@ public class Server {
     }
 }
 
+// thread for multiple connection
 class serverThread extends Thread{
     private Socket socket;
     private TicTacToeGame game;
@@ -30,29 +36,38 @@ class serverThread extends Thread{
     private String playerName = "";
     private boolean inGame = false;
 
+    // constructor for setting connection and sharing game
     public serverThread(Socket socket, TicTacToeGame game){
         this.socket = socket;
         this.game = game;
     }
     public void run() {
         try{
+            // set up for the sending and receiving message
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
             String message = " ";
+            // connection start at the room "lobby"
             String place = "lobby";
             while(true){
+                // getting client sending message
                 buffer = inFromClient.readLine();
+                // different mode for the client sending message
                 String mode = buffer.split(" ")[0];
                 String info = "";
                 if(buffer.split(" ").length>=2)
                     info = buffer.split(" ")[1];
-                System.out.println(buffer);
-                message = game.message;
+                //System.out.println(buffer);
+                //message = game.message;
+
+                // login mode, add a new player into this game
                 if(mode.equals("login")){
                     game.addNewPlayer(info);
                     playerName = info;
                 }
+                // play mode
                 else if(mode.equals("play")){
+                    // first need to join into the game, then change the position into game room
                     if(!inGame){
                         game.join(playerName);
                         game.reset();
@@ -60,52 +75,63 @@ class serverThread extends Thread{
                         place = "room";
                     }
                     else{
+                        // play mode, but other message is about what action you want to do in the game
                         if(game.isMyTurn(playerName) && game.checkWin().equals("no winner")){
                             int row = Integer.parseInt(info.split(",")[0]);
                             int col = Integer.parseInt(info.split(",")[1]);
                             String type = game.getPlayerType(playerName);
                             String win = game.setMove(row, col, type);
                             if(!win.equals("no winner")){
+                                // this player win the game
                                 if(win.equals(playerName)){
-                                    message = playerName + " win.";
+                                    game.message = "You win the game.";
                                     //System.out.println(playerName+" win.");
+                                    
+                                    // set the win/lose on user list, and go back to the lobby
                                     game.setPlayerList(playerName, String.valueOf(Integer.parseInt(game.getPlayerWin(playerName))+1), game.getPlayerLose(playerName), game.getPlayerOnline(playerName));
-                        
                                     inGame = false;
                                     place = "lobby";
                                 }
                             }
                         }
                         else{
-                            message = "This is not "+playerName+" turn.";
+                            game.message = "This is not your turn.";
                             //System.out.println("This is not "+playerName+" turn.");
                         }
                     }
                 }
+                // end mode, shutdown the connection
                 else if(mode.equals("end")){
                     outToClient.writeBytes("end/ / " + '\n');
+                    // set the user list of this player into offline
                     game.setPlayerList(playerName, game.getPlayerWin(playerName), game.getPlayerLose(playerName), "0");
                     socket.close();
                     break;
                 }
                 else{
+                    // you notice you opponent win the game
                     if(inGame && !game.checkWin().equals("no winner")){
                         inGame = false;
+                        // set the win/lose on user list
                         game.setPlayerList(playerName, game.getPlayerWin(playerName), String.valueOf(Integer.parseInt(game.getPlayerLose(playerName))+1), game.getPlayerOnline(playerName));
+                        game.message = "You lose the game.";
+                        // go back to the lobby
                         place = "lobby";
                         game.leave();
                     }
                 }
-
+                message = game.message;
+                // different message for different room
                 if(inGame){
-                    System.out.println(game.showMap());
+                    //System.out.println(game.showMap());
                     outToClient.writeBytes(place+"/"+message+"/"+game.showMap() + "\n");
                 }
                 else{
-                    System.out.println(game.showPlayerList());
-                    outToClient.writeBytes(place+"/"+message+"/"+game.showPlayerList() + "\n");
+                    //System.out.println(game.showPlayerList());
+                    outToClient.writeBytes(place+"/Message: "+message+"/"+game.showPlayerList() + "\n");
                 }
-                game.message = " ";
+                if(game.message.length()>=10 && !game.message.split(" ")[0].equals("Player"))
+                    game.message = " ";
             }
         }
         catch(Exception e){
@@ -114,6 +140,7 @@ class serverThread extends Thread{
     }
 }
 
+// sharing game class
 class TicTacToeGame{
     private ArrayList<String> userList = new ArrayList<String>();
     private int nowUserCount;
@@ -123,15 +150,25 @@ class TicTacToeGame{
     public String message = " ";
     int MAX_USER = 2;
 
+    // constructor to set the number of user is playing, the player name, and the gaming map
     public TicTacToeGame(){
         nowUserCount = 0;
         player = new String[2];
         map = new String[3][3];
         
     }
+    // add a new player, but if it was an old user login before, just change the offline into online
     public void addNewPlayer(String playerName){
+        for(int i=0;i<userList.size();i++){
+            if(userList.get(i).split(",")[0].equals(playerName)){
+                setPlayerList(playerName, getPlayerWin(playerName), getPlayerLose(playerName), "1");
+                return;
+            }
+        }
+        // not find in the user list, so add the new user in it
         userList.add(playerName+",0,0,1");
     }
+    // get the player win data
     public String getPlayerWin(String playerName){
         for(int i=0;i<userList.size();i++){
             if(userList.get(i).split(",")[0].equals(playerName)){
@@ -140,6 +177,7 @@ class TicTacToeGame{
         }
         return "";
     }
+    // get the player lose data
     public String getPlayerLose(String playerName){
         for(int i=0;i<userList.size();i++){
             if(userList.get(i).split(",")[0].equals(playerName)){
@@ -148,6 +186,7 @@ class TicTacToeGame{
         }
         return "";
     }
+    // get the player online data
     public String getPlayerOnline(String playerName){
         for(int i=0;i<userList.size();i++){
             if(userList.get(i).split(",")[0].equals(playerName)){
@@ -156,6 +195,7 @@ class TicTacToeGame{
         }
         return "";
     }
+    // set the player data
     public void setPlayerList(String playerName, String win, String lose, String online){
         for(int i=0;i<userList.size();i++){
             if(userList.get(i).split(",")[0].equals(playerName)){
@@ -163,6 +203,7 @@ class TicTacToeGame{
             }
         }
     }
+    // show the whole player list in indicated format
     public String showPlayerList(){
         String temp = "";
         for(int i=0;i<userList.size();i++){
@@ -170,11 +211,13 @@ class TicTacToeGame{
         }
         return temp;
     }
+    // synchronized function for joining the game
     public synchronized void join(String playerName){
         message = "Waiting for other player joining...";
         //System.out.println("Waiting for other player joining...");
+        
+        // the room is full, just waiting...
         while(nowUserCount == MAX_USER){
-            message = "Playing room is full.";
             try{
                 wait();
             }
@@ -183,21 +226,26 @@ class TicTacToeGame{
                 System.exit(0);
             }
         }
+        // set the playing user name
         player[nowUserCount++] = playerName;
         notifyAll();
         message = "Player "+playerName+" join in the game.";
-        System.out.println("Player "+playerName+" join in the game.");
+        //System.out.println("Player "+playerName+" join in the game.");
     }
+    // leave the game
     public void leave(){
         nowUserCount = 0;
         player = new String[2];
+        map = new String[3][3];
     }
+    // reset the gaming map
     public void reset(){
         turn = 0;
         for(int i=0;i<3;i++)
             for(int j=0;j<3;j++)
                 map[i][j]= " ";
     }
+    // get the gaming map
     public String showMap(){
         String temp = "";
         for(int i=0;i<3;i++)
@@ -205,6 +253,7 @@ class TicTacToeGame{
                 temp += map[i][j];
         return temp;
     }
+    // set the move on the map for the position
     public String setMove(int row, int col, String type){
         if(map[row][col].equals(" ")){
             turn = (turn+1)%2;
@@ -215,18 +264,21 @@ class TicTacToeGame{
         //System.out.println("Illegal move.");
         return "";
     }
+    // get the player type, "O" or "X"
     public String getPlayerType(String playerName){
         if(playerName.equals(player[0]))
             return "O";
         else
             return "X";
     }
+    // check this turn is this player or not
     public boolean isMyTurn(String playerName){
         if(player[turn].equals(playerName))
             return true;
         else
             return false;
     }
+    // check the game has a winner or not
     String checkWin(){
         if(map[0][0].equals("O") && map[0][1].equals("O") && map[0][2].equals("O") || map[1][0].equals("O") && map[1][1].equals("O") && map[1][2].equals("O") || map[2][0].equals("O") && map[2][1].equals("O") && map[2][2].equals("O"))
             return player[0];
